a58d45d1dbecb8267ee6a8cd476faaa38a842327
Amir <amir.hackett@gmail.com>
2022-04-06 18:13:16 -0400

    edited js

--- assets/Drink Experiment/drinks.js
+++ assets/Drink Experiment/drinks.js
@@ -1,22 +1,37 @@
-var drinkRecipe = function(repo) {
-    // format the github api url
-    var apiUrl = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-  
-    // make a get request to url
-    fetch(apiUrl).then(function(response) {
-      // request was successful
-      if (response.ok) {
-        response.json().then(function(data) {
-          displayDrinks(data);
-  
-          // check if api has paginated issues
-          if (response.headers.get("Link")) {
-            displayWarning(repo);
-          }
-        });
-      } else {
-        // if not successful, redirect to homepage
-        document.location.replace("./index.html");
+function getCocktail(){
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  .then(
+  function(response) {
+      if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+      return;
       }
-    });
-  };
+ 
+      // Examine the text in the response
+      response.json().then(function(data) {
+      console.log(data);
+      displayCocktail(data)
+      });
+  }
+  )
+  .catch(function(err) {
+  console.log('Fetch Error :-S', err);
+  }); 
+}
+
+getCocktail()
+
+
+function displayCocktail(cocktail){
+  console.log(cocktail.drinks[0].strDrink)
+  console.log(cocktail.drinks[0].strIngredient1)
+  console.log(cocktail.drinks[0].strMeasure1)
+  console.log(cocktail.drinks[0].strIngredient2)
+  console.log(cocktail.drinks[0].strMeasure2)
+  console.log(cocktail.drinks[0].strIngredient3)
+  console.log(cocktail.drinks[0].strMeasure3)
+  console.log(cocktail.drinks[0].strIngredient4)
+  console.log(cocktail.drinks[0].strInstructions)
+}
+
