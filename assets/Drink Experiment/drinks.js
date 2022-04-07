48f79fb576e7a36c2fba365f85dbb0b39ea60c32
Amir <amir.hackett@gmail.com>
2022-04-06 23:40:41 -0400

    edited drink js

--- assets/Drink Experiment/drinks.js
+++ assets/Drink Experiment/drinks.js
@@ -1,3 +1,6 @@
+//var drinkName = document.querySelector(".content")
+var searchResultsContainer = document.querySelector('.content')
+
 function getCocktail(){
   fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
   .then(
@@ -11,7 +14,7 @@ function getCocktail(){
       // Examine the text in the response
       response.json().then(function(data) {
       console.log(data);
-      displayCocktail(data)
+      loadCards(data)
       });
   }
   )
@@ -24,14 +27,62 @@ getCocktail()
 
 
 function displayCocktail(cocktail){
-  console.log(cocktail.drinks[0].strDrink)
-  console.log(cocktail.drinks[0].strIngredient1)
-  console.log(cocktail.drinks[0].strMeasure1)
-  console.log(cocktail.drinks[0].strIngredient2)
-  console.log(cocktail.drinks[0].strMeasure2)
-  console.log(cocktail.drinks[0].strIngredient3)
-  console.log(cocktail.drinks[0].strMeasure3)
-  console.log(cocktail.drinks[0].strIngredient4)
-  console.log(cocktail.drinks[0].strInstructions)
+  for(i = 0; i < cocktail.drinks.length; i++){
+
+
+  console.log(cocktail.drinks[i].strDrink)
+  console.log(cocktail.drinks[i].strIngredient1)
+  console.log(cocktail.drinks[i].strMeasure1)
+  console.log(cocktail.drinks[i].strIngredient2)
+  console.log(cocktail.drinks[i].strMeasure2)
+  console.log(cocktail.drinks[i].strIngredient3)
+  console.log(cocktail.drinks[i].strMeasure3)
+  console.log(cocktail.drinks[i].strIngredient4)
+  console.log(cocktail.drinks[i].strInstructions)
+  }
 }
 
+function loadCards(data){
+  // for (var i = 0; i < 3; i++){
+  //   var drinkName = document.createElement("h4")
+  //   drinkName.innerHTML = data.drinks[i].strDrink
+  //   searchResultsContainer.append(drinkName)
+  // }
+
+  for (var i = 1; i < 3; i++) {
+    for(var x = 0; x < data.drinks.length; x++){
+      var cocktailIngredient = data.drinks[x]['strIngredient' + i]
+     var drinkName = document.getElementsByTagName("h4")
+     var ingredient = document.createElement("p")
+     ingredient.innerHTML = cocktailIngredient
+     drinkName.appendChild(ingredient)
+    var cocktail = data;
+      console.log(cocktailIngredient)
+    if(!cocktailIngredient){
+      break
+    }
+    console.log(cocktailIngredient)
+   searchResultsContainer.innerHTML += `
+    
+    <div class="card is-shady column is-3">
+      <div class="card-image has-text-centered">
+        <i class="fa-solid fa-utensils"></i>
+      </div>
+      
+      <div class="card-content">
+        <div class="content">
+          <img src="${cocktail.drinks[x].strDrinkThumb}"/>
+          <h4>${cocktail.drinks[x].strDrink}</h4>
+          <p>
+            ${data.drinks[x]['strIngredient' + i]}
+          </p>
+          <!--<p><a href="${cocktail.drinks[x].strMeasure1}">See Full Recipe</a></p>-->
+        </div>
+      </div>
+
+    </div>
+  
+    `
+  }
+  }
+}
